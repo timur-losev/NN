acd4690930247bd539931390ce13b1df4a3f4397
Timur Losiev <timur.l@ilyon.net>
2019-02-11 20:44:12 +0200

    - init

---
+++ PartyNN.py
@@ -0,0 +1,23 @@
+import numpy as np
+import sys
+
+class PartyNN(object):
+    def __init__(self, learning_rate=0.1):
+        self.weights_0_1 = np.random.normal(0.0, 2 ** -0.5, (2, 3))
+        self.weights_1_2 = np.random.normal(0.0, 1, (1, 2))
+        self.sigmoid_mapper = np.vectorize(self.sigmoid)
+        self.learning_rate = np.array([learning_rate])
+
+    def sigmoid(self, x):
+        return 1 / (1 + np.exp(-x))
+
+    def predict(self, inputs):
+        inputs_1 = np.dot(self.weights_0_1, inputs)
+        outputs_1 = self.sigmoid_mapper(inputs_1)
+
+        inputs_2 = np.dot(self.weights_1_2, outputs_1)
+        outputs_2 = self.sigmoid_mapper(inputs_2)
+    
+
+if __name__ == "__main__":
+    network = PartyNN(0.08)
