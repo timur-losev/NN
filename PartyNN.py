2c33da1103238b2227b46c6a29e9494366d0f48d
Timur Losiev <timur.l@ilyon.net>
2019-02-13 15:09:18 +0200

    - update

--- PartyNN.py
+++ PartyNN.py
@@ -1,6 +1,9 @@
 import numpy as np
 import sys
 
+def MSE(y, Y):
+    return np.mean((y-Y)**2)
+
 class PartyNN(object):
     def __init__(self, learning_rate=0.1):
         self.weights_0_1 = np.random.normal(0.0, 2 ** -0.5, (2, 3))
@@ -17,7 +20,52 @@ class PartyNN(object):
 
         inputs_2 = np.dot(self.weights_1_2, outputs_1)
         outputs_2 = self.sigmoid_mapper(inputs_2)
-    
+        return outputs_2
+
+    def train(self, inputs, expected_predict):
+        inputs_1 = np.dot(self.weights_0_1, inputs)
+        outputs_1 = self.sigmoid_mapper(inputs_1)
+
+        inputs_2 = np.dot(self.weights_1_2, outputs_1)
+        outputs_2 = self.sigmoid_mapper(inputs_2)
+        actual_predict = outputs_2[0]
+
+        error_layer_2 = np.array([actual_predict - expected_predict])
+        gradient_layer_2 = actual_predict * (1 - actual_predict)
+        weights_delta_layer_2 = error_layer_2 * gradient_layer_2
+        self.weights_1_2 -= (np.dot(weights_delta_layer_2, outputs_1.reshape(1, len(outputs_1)))) * self.learning_rate
+
+        error_layer_1 = weights_delta_layer_2 * self.weights_1_2
+        gradient_layer_1 = outputs_1 * (1 - outputs_1)
+        weights_delta_layer_1 = error_layer_1 * gradient_layer_1
+        self.weights_0_1 -= np.dot(inputs.reshape(len(inputs), 1), weights_delta_layer_1).T  * self.learning_rate
+
 
 if __name__ == "__main__":
     network = PartyNN(0.08)
+
+    train = [
+        ([0, 0, 0], 0),
+        ([0, 0, 1], 1),
+        ([0, 1, 0], 0),
+        ([0, 1, 1], 0),
+        ([1, 0, 0], 1),
+        ([1, 0, 1], 1),
+        ([1, 1, 0], 0),
+        ([1, 1, 1], 0),
+    ]
+
+    epochs = 6000
+    for e in range(epochs):
+        inputs_ = []
+        correct_predictions = []
+        for input_stat, correct_predict in train:
+            network.train(np.array(input_stat), correct_predict)
+            inputs_.append(np.array(input_stat))
+            correct_predictions.append(np.array(correct_predict))
+        
+        train_loss = MSE(network.predict(np.array(inputs_).T), np.array(correct_predictions))
+
+        sys.stdout.write("\\rProgress: {}, Training loss: {}".format(str(100 * e/float(epochs))[:4], str(train_loss)[:5]))
+        
+
